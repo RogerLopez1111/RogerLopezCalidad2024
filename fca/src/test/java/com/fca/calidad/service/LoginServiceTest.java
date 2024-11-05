d3204f2fb2e227b8f8836b49230aa8e001816a1e
Roger Lopez <rogerlopez1111@gmail.com>
2024-11-04 18:29:09 -0600

    4 de November

--- fca/src/test/java/com/fca/calidad/service/LoginServiceTest.java
+++ fca/src/test/java/com/fca/calidad/service/LoginServiceTest.java
@@ -25,6 +25,36 @@ when (dao.findByUserName("nombre1")).thenReturn(usuario);
 boolean result = service.login("nombre1", "123");
 //verificacion
 assertThat(result,is(true));
+
+
 	}
+	
+	@Test
+	void test2() {
+		//inicializacion
+dao=mock (IDAOUser.class);
+service=new LoginService(dao);
+User usuario = new User ("nombre1","email","123");
+when (dao.findByUserName("nombre1")).thenReturn(usuario);
+
+//ejercicio 2 contraseña incorrecta
+boolean result2 = service.login("nombre1", "321");
+//verificacion
+assertThat(result2,is(false));
+}
+	@Test
+	void test3() {
+		//inicializacion
+dao=mock (IDAOUser.class);
+service=new LoginService(dao);
+User usuario = new User ("nombre1","email","123");
+when (dao.findByUserName("nombre1")).thenReturn(usuario);
+
+//ejercicio 3 usuario no encontrado
+boolean result = service.login("", "321");
+//verificacion
+assertThat(result,is(false));
+}
+	
 
 }
