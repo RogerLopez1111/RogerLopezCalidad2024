6b5256519ac50436b0cacf6606316b1906787887
Roger Lopez <rogerlopez1111@gmail.com>
2024-11-21 08:32:51 -0600

    conexion a la base de datos, app.java realiza las operaciones de la base de datos

---
+++ fca/src/test/java/com/fca/calidad/integracion/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.fca.calidad.integracion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
